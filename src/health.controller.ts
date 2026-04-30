96a278cfc6d2496ee705b7cf0f89d7459bc97670
dilshod.sirojov <dilshod.sirojov@mf.uz>
2026-04-30 17:21:48 +0500

    hot fix: notification

--- src/health.controller.ts
+++ src/health.controller.ts
@@ -5,6 +5,7 @@ export class HealthController {
   @Get()
   check() {
     // Cron-job.org shunchaki 200 OK javobini kutadi
+    console.log('Bot is alive');
     return {
       status: 'ok',
       timestamp: new Date().toISOString(),
